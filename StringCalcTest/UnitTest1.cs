008460b65cc980d82a13539e0cac14c7a92e12ac
Ibrahimaden00 <Ibrahimaden00@gmail.com>
2021-12-06 16:33:39 +0100

    Added single number Test.

--- StringCalcTest/UnitTest1.cs
+++ StringCalcTest/UnitTest1.cs
@@ -8,12 +8,21 @@ namespace StringCalculator
     public class UnitTest1
     {
         [Fact]
-        public void Add_EmptyString_ShouldReturnZero()
+        public void Test_Add_EmptyString_ShouldReturnZero()
         {
             var expected = 0;
 
             var result = Calculator.Add("");
 
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Add_SingleNumber_ShouldReturnSingleNumber()
+        {
+            var expected = 1;
+
+            var result = Calculator.Add("1");
             Assert.Equal(expected, result);
         }
     }
